79e82c53c9f18ebcc9d738d661465ee357395db2
Zach Hoy <hoyzach@gmail.com>
2022-02-18 13:46:26 -0500

    initial storage and functionality set-up

--- Voting_Booth.sol
+++ Voting_Booth.sol
@@ -1,8 +1,50 @@
 // SPDX-License-Identifier: Unlicensed
+
+/*
+Even though voter data is visible on the Ethereum blockchain, this application is written as if it isn't.  
+The ultimate decentralized voting application would not allow anyone to see voting data as it is collected,
+to discourage voting based on current voting data and waiting to vote until the last minute.
+
+This application does not prevent the same entity from creating multiple address to vote from.
+A separate database with a voter identifcation number obtained through a voter identification verification
+process would need to be queried through a modified register function. 
+*/
 pragma solidity ^0.8.6;
 
 import "Voting_Storage.sol";
 
 contract VotingBooth is VotingStorage {
-    
+
+    function register() public {
+        require(_registry[msg.sender] != true);
+        _registry[msg.sender] = true;
+        _voterCount++;
+    }
+
+    function setCategory(string memory _category, string[] memory _candidates) public onlyOwner {
+
+    }
+
+    function castVote(string memory _category, string memory _candidate) public registered {
+        require(_boolVoter[msg.sender][_category] == false);
+        _boolVoter[msg.sender][_category] = true;
+        //add vote for cadidate
+        //add vote to totalVotes
+        //add vote for totalCategoryVotes
+    }
+
+        //implement a timer of some sort
+    function setTimer(uint) public onlyOwner {
+
+    }
+
+    //collect votes when timer is comeplete
+    function getCategoryVotes(string memory _candidate) public returns(uint) {
+            // require timer to be complete
+    }
+    function getTotalVotes() public returns(uint){
+
+    }
+
+
 }
--- Voting_Storage.sol
+++ Voting_Storage.sol
@@ -5,6 +5,28 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VotingStorage is Ownable{
-    
 
+    modifier registered() {
+        require(_registry[msg.sender] == true);
+        _;
+    }
+
+    struct Candidate {
+        string _candidate;
+        uint _numVotes;
+    }
+
+    // address => bool --- list of voter addresses registered to site
+    mapping (address => bool) _registry;
+    // address => category => bool --- 1 vote per voter per category
+    mapping (address => mapping(string => bool)) _boolVoter;
+    // category => candidates struct
+    mapping (string => Candidate[]) _categories;
+    mapping (string => bool) _open;
+
+    // total voters registered
+    uint64 public _voterCount;
+    // total votes recorded on application
+    uint64 public _totalVotes;
+    
 }
