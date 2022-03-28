72bf63aca88db993a5225cfbe7109f05bd7cfce3
Zach Hoy <hoyzach@gmail.com>
2022-03-28 17:44:41 -0400

    update config with test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,15 @@
 
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
+let secret = require("./secret");
 //require('@openzeppelin/hardhat-upgrades');
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: secret.url,
+      accounts: [secret.key]
+    }
+  }
 };
